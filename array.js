f43fd3ccb01ba2207d6e740da28204a3d7af396c
unknown <nabilmasudh@gmail.com>
2021-09-12 11:40:04 +0600

    this file modified

--- array.js
+++ array.js
@@ -1,16 +1,62 @@
-var isArray = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80]
+// var isArray = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80]
 
-var find  = 10
-var isFound = false;
+// var find  = 10
+// var isFound = false;
 
-for(var i = 0; i < isArray.length; i++){
-    if(isArray[i] === find){
-        console.log('Data Found Here');
-        isFound = true;
-        break;
-    }
-}
+// for(var i = 0; i < isArray.length; i++){
+//     if(isArray[i] === find){
+//         console.log('Data Found Here');
+//         isFound = true;
+//         break;
+//     }
+// }
 
-if(!isFound){
-    console.log('Data Not Found Here')
-}
+// if(!isFound){
+//     console.log('Data Not Found Here')
+// }
+
+// reverse Array ===========================
+
+// let isArray = [1, 2, 3, 3, 4, 5, 6, 7, 8]
+
+// for(let i = 0; i < (isArray.length / 2); i++){
+//     let tempArray = isArray[i]
+//     isArray[i] = isArray[isArray.length - 1 - i]
+//     isArray[isArray.length - 1 - i] = tempArray;
+    
+// }
+// for(let x = 0; x <isArray.length; x++){
+//     console.log(isArray[x])
+// }
+
+
+// let muliArray = [
+//     [
+//         [1, 2, 3, 4],
+//         [5, 6, 7, 8],
+//     ],
+//     [
+//         [5, 6, 7, 8],
+//         [9, 6, 3, 5],
+//     ],
+// ]
+
+// for(let i = 0; i < muliArray.length; i++){
+//     for(let j = 0; j < muliArray[i].length; j++){
+//         for(let k = 0; k < muliArray[i][j].length; k++){
+//             console.log('First Element ' + i + ': '+ 'Second Element ' + j + ': ' + muliArray[i][j][k])
+//         }
+//     }
+// }
+
+// Array methods ==================================
+
+let isArray = [1, 2, 3, 3, 4, 5]
+let isArray2 = [1, 2, 3, 3, 4, 5]
+
+console.log(isArray.join('/ '))
+// console.log(isArray.fill(4))
+
+let isArray3 = isArray.concat(isArray2)
+console.log(isArray3)
+
